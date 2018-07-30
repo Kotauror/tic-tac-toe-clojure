64b00bf9781899a3e76cacf1ebff44f61576ad97
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-07-30 12:08:29 +0100

    Setup the project.

---
+++ spec/clj/tic_tac_toe/core_spec.clj
@@ -0,0 +1,7 @@
+(ns tic-tac-toe.core-spec
+  (:require [speclj.core :refer :all]
+            [tic-tac-toe.core :refer :all]))
+
+(describe "A Clojure test"
+  (it "fails. Fix it!"
+    (should= 0 1)))
---
+++ spec/cljs/tic_tac_toe/core_spec.cljs
@@ -0,0 +1,8 @@
+(ns tic-tac-toe.core-spec 
+  (:require-macros [speclj.core :refer [describe it should=]])
+  (:require [speclj.core]
+            [tic-tac-toe.core]))
+
+(describe "A ClojureScript test"
+  (it "fails. Fix it!"
+    (should= 0 1)))
