6042b57becfa5e1f724e3f2f6d635953ff2bb086
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-07-31 12:13:14 +0100

    Feature & Refactor: add prompts ns to store user prompts.

--- src/clj/tic_tac_toe/human_player.clj
+++ src/clj/tic_tac_toe/human_player.clj
@@ -1,8 +1,9 @@
 (ns tic_tac_toe.human_player
   (:require [tic_tac_toe.ui :refer [print-prompt
-                                    get-user-input]]))
+                                    get-user-input]]
+            [tic_tac_toe.prompts :refer [pick-position-prompt]]))
 
 (defn pick-position [board]
-  (print-prompt "pick position")
+  (print-prompt pick-position-prompt)
   (get-user-input))
   
---
+++ src/clj/tic_tac_toe/prompts.clj
@@ -0,0 +1,3 @@
+(ns tic_tac_toe.prompts)
+
+(def pick-position-prompt "Pick position")
