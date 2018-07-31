3596edfa173435f80d657e98ce3d209eb35979e2
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-07-31 16:19:00 +0100

    Feature & test: play-turn loop in progress.

--- spec/clj/tic_tac_toe/game-spec.clj
+++ spec/clj/tic_tac_toe/game-spec.clj
@@ -4,13 +4,14 @@
             [tic_tac_toe.ui :refer [show-board
                                     print-prompt]]
             [tic_tac_toe.board :refer [get-rows
+                                       is-over?
                                        create-board]]
             [tic_tac_toe.game :refer [run-game
                                       play-turn]]))
 
 (describe "run-game"
   (with-stubs)
-  (it "calls play turn with a board"
+  (it "calls right functions"
     (with-redefs [
       print-prompt (stub :print-prompt-stub)
       play-turn (stub :play-turn-stub)
@@ -28,8 +29,9 @@
 
 (describe "play-turn"
   (with-stubs)
-  (it "shows the board"
+  (it "prints the output if the game is over"
     (with-redefs [
+      is-over? (stub :is-over-stub {:return true})
       pick-position (stub :pick-position-stub)
       get-rows (stub :get-rows-stub {:return :rows})
       show-board (stub :show-board-stub)]
@@ -42,7 +44,12 @@
           [:rows]})
 
       (should-have-invoked
-        :pick-position-stub
+        :is-over-stub
         {:with 
-          [[1 2 3 4 5 6 7 8 9]]}))))
+        [[1 2 3 4 5 6 7 8 9]]}))))
+
+      ;(should-have-invoked
+       ; :pick-position-stub
+       ; {:with 
+        ;  [[1 2 3 4 5 6 7 8 9]]}))))
 
--- src/clj/tic_tac_toe/game.clj
+++ src/clj/tic_tac_toe/game.clj
@@ -1,6 +1,8 @@
 (ns tic_tac_toe.game
   (:require [tic_tac_toe.board :refer [get-rows
-                                       create-board]]
+                                       is-over?
+                                       create-board
+                                       put-sign-on-board]]
             [tic_tac_toe.human_player :refer [pick-position]]
             [tic_tac_toe.ui :refer [print-prompt
                                     show-board]]
@@ -8,7 +10,10 @@
 
 (defn play-turn [board]
   (show-board (get-rows board))
-  (pick-position board))
+  (cond 
+    (is-over? board) (println "Over")
+    :else (do 
+      (recur (put-sign-on-board board (pick-position board) "X")))))
 
 (defn run-game []
   (print-prompt hello-prompt)
