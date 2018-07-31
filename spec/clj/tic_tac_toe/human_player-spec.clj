16bb1a7ae859c5a90691957865348785b81e0ca0
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-07-31 13:49:12 +0100

    Test: add test for invalid user input.

--- spec/clj/tic_tac_toe/human_player-spec.clj
+++ spec/clj/tic_tac_toe/human_player-spec.clj
@@ -3,7 +3,12 @@
             [tic_tac_toe.human_player :refer [pick-position]]))
 
 (describe "pick-position"
-  (it "picks valid position"
+  (it "returns position on valid input"
     (should= "1"
       (with-in-str "1"
-        (pick-position [1 2 3 4 5 6 7 8 9])))))
+        (pick-position [1 2 3 4 5 6 7 8 9]))))
+  (it "returns valid input of the user"
+    (should= "2"
+        (with-in-str "1\n2"
+          (pick-position ["X" 2 3 4 5 6 7 8 9])))))
+
