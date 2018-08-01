88a52ab64abfc1fe9ab23c705b278ec71b01bc21
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-08-01 15:27:35 +0100

    ! Broken minimax.

--- spec/clj/tic_tac_toe/computer-spec.clj
+++ spec/clj/tic_tac_toe/computer-spec.clj
@@ -3,6 +3,7 @@
             [tic_tac_toe.signs :refer [player-one-sign
                                        player-two-sign]]
             [tic_tac_toe.computer :refer [calculate-score
+                                          score-moves
                                           minimax]]))
 
 (describe "calculate-score"
@@ -13,7 +14,12 @@
     (should= -6
     (calculate-score ["Y" "Y" "Y" 4 5 "Y" 7 8 9] 4 "X" "Y"))))
 
-(describe "minimax"
-  (it "should return top-left corner as the initial position")
-    (should= 1
-    (minimax [1 2 3 4 5 6 7 8 9] 0 player-one-sign player-two-sign)))
+(describe "score-moves"
+  (it "creates a hash-map with scores for moves"
+    (should= {1 10, 4 7, 7 4}
+    (score-moves [1 "X" "X" 4 "O" "O" 7 "X" "O"] "X" "Y" 0))))
+
+;(describe "minimax"
+;  (it "should return top-left corner as the initial position")
+ ;   (should= 1
+  ;  (minimax [1 2 3 4 5 6 7 8 9] 0 player-one-sign player-two-sign)))
--- src/clj/tic_tac_toe/computer.clj
+++ src/clj/tic_tac_toe/computer.clj
@@ -1,7 +1,10 @@
 (ns tic_tac_toe.computer
-  (:require [tic_tac_toe.board :refer [winner?
+  (:require [tic_tac_toe.signs :refer [switch-signs]]
+            [tic_tac_toe.board :refer [winner?
+                                       get-free-places
                                        put-sign-on-board
                                        is-tie?]]))
+(declare score-moves)
 
 (defn calculate-score [board level active-sign passive-sign]
   (cond 
@@ -9,4 +12,30 @@
     (winner? passive-sign board) (- (- 10 level)) 
     (is-tie? board) 0 ))
 
-(defn minimax [board level active-sign passive-sign])
+(defn best-move-and-score [depth moves] 
+  (if (= (mod depth 2) 0)
+    (apply max-key val moves)
+    (apply min-key val moves)))
+
+(defn best-score-at-depth [depth moves]
+  (println depth)
+  (println moves)
+  (key (best-move-and-score depth moves)))
+
+(defn best-move [moves]
+  (key (apply max-key val moves)))
+
+(defn get-score [board active-sign passive-sign depth]
+  (cond (winner? active-sign board) (- 10 depth)
+        (winner? passive-sign board) (- (- 10 depth))
+        (is-tie? board) 0
+        :else (best-score-at-depth depth (score-moves board (switch-signs active-sign) (switch-signs passive-sign) (inc depth)))))
+
+(defn score-moves [board active-sign passive-sign depth]
+  (let [
+       moves (get-free-places board)
+       scores (map #(get-score (put-sign-on-board board (str %) active-sign) active-sign passive-sign depth) moves)]
+  (zipmap moves scores)))
+
+(defn minimax [board level active-sign passive-sign]
+  (best-move (score-moves board active-sign passive-sign 0)))
