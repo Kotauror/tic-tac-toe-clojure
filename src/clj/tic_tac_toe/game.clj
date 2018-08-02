704477fa36e705a657f9e9ce0b11ecf2d48c4482
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-08-02 14:26:32 +0100

    Feature & Tests: human plays against minimax.

--- src/clj/tic_tac_toe/game.clj
+++ src/clj/tic_tac_toe/game.clj
@@ -10,23 +10,37 @@
                                        player-two-sign
                                        switch-signs]]
             [tic_tac_toe.human_player :refer [pick-position]]
+            [tic_tac_toe.computer :refer [minimax]]
             [tic_tac_toe.ui :refer [print-final-result
                                     print-prompt
                                     show-board
                                     inform-of-move]]
             [tic_tac_toe.prompts :refer [hello-prompt]]))
 
-(defn play-turn [board]
+(declare play-turn)
+
+(defn human-move [board level]
+  (let [picked-position (pick-position board)
+  current-player (get-current-mark board)]
+  (inform-of-move current-player picked-position)
+  (play-turn (put-sign-on-board board picked-position current-player) (inc level))))
+
+(defn computer-move [board level]
+  (let [picked-position (minimax board)
+  current-player (get-current-mark board)]
+  (inform-of-move current-player (str picked-position))
+  (play-turn (put-sign-on-board board (str picked-position) current-player) (inc level))))
+
+(defn play-turn [board level]
   (show-board (get-rows board))
   (cond 
     (is-over? board) (print-final-result (get-winner-sign board))
     :else (do
-      (let [picked-position (pick-position board)
-            current-player (get-current-mark board)]
-        (inform-of-move current-player picked-position)
-        (recur (put-sign-on-board board picked-position current-player))))))
+      (cond 
+        (= (mod level 2) 0) (human-move board level)
+        (not= (mod level 2) 0) (computer-move board level)))))
 
 (defn run-game []
   (print-prompt hello-prompt)
-  (play-turn (create-board)))
+  (play-turn (create-board) 0))
 
