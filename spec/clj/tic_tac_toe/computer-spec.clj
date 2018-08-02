027b12e080d3477331af7bd2c3e924e8ba50dab5
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-08-02 09:28:49 +0100

    Feature & Tests: minimax ready, needs refactoring.

--- spec/clj/tic_tac_toe/computer-spec.clj
+++ spec/clj/tic_tac_toe/computer-spec.clj
@@ -3,24 +3,27 @@
             [tic_tac_toe.signs :refer [player-one-sign
                                        player-two-sign]]
             [tic_tac_toe.computer :refer [
-                                          ;calculate-score
                                           score-moves
                                           minimax]]))
 
-;(describe "calculate-score"
-;  (it "calculates score for terminal board - active player"
-;    (should= 6
-;    (calculate-score ["X" "X" "X" 4 5 "Y" 7 8 9] 4 "X" "Y")))
-;  (it "calculates score for terminal board - passive player"
-;    (should= -6
-;    (calculate-score ["Y" "Y" "Y" 4 5 "Y" 7 8 9] 4 "X" "Y"))))
-
 (describe "score-moves"
-  (it "creates a hash-map with scores for moves"
-    (should= {1 9, 4 7, 7 (- 8)}
-    (score-moves [1 "X" "X" 4 "O" "O" 7 "X" "O"] "X" "O" 0))))
+  (it "creates a hash-map with scores for moves - win/lose positions"
+    (should= {1 9, 4 (- 8), 7 (- 8)}
+    (score-moves [1 "X" "X" 4 "O" "O" 7 "X" "O"] "X" "O" 0)))
+  (it "creates a hash-map with scores for moves - tie positions"
+    (should= {3 0, 4 0, 9 9}
+    (score-moves ["X" "O" 3 4 "X" "O" "O" "X" 9] "X" "O" 0))))
 
-;(describe "minimax"
- ; (it "should return top-left corner as the initial position")
-  ;  (should= 1
-   ; (minimax [1 2 3 4 5 6 7 8 9] 0 player-one-sign player-two-sign)))
+(describe "minimax"
+  (it "X takes winning position"
+    (should= 6
+     (minimax ["X" "O" "X" 4 "O" 6 "O" "X" "X"] 0 "X" "O")))
+  (it "O takes winning position"
+    (should= 6
+     (minimax ["X" "X" 3 "O" "O" 6 "X" 8 9] 0 "O" "X")))
+  (it "X prevents O from winning"
+    (should= 6
+      (minimax ["X" "X" "O" 4 5 6 7 8 "O"] 0 "X" "O")))
+  (it "O prevents X from winning"
+    (should= 6
+      (minimax ["O" "O" "X" 4 5 6 7 8 "X"] 0 "O" "X"))))
--- src/clj/tic_tac_toe/computer.clj
+++ src/clj/tic_tac_toe/computer.clj
@@ -6,37 +6,32 @@
                                        is-tie?]]))
 (declare score-moves)
 
-;(defn calculate-score [board level active-sign passive-sign]
-;  (cond 
-;    (winner? active-sign board) (- 10 level)
-;    (winner? passive-sign board) (- (- 10 level)) 
-;    (is-tie? board) 0 ))
-
 (defn best-move-and-score [depth moves] 
   (if (= (mod depth 2) 0)
     (apply max-key val moves)
     (apply min-key val moves)))
 
 (defn best-score-at-depth [depth moves]
-  (println depth)
-  (println moves)
   (val (best-move-and-score depth moves)))
 
 (defn best-move [moves]
   (key (apply max-key val moves)))
 
 (defn get-score [board active-sign passive-sign depth]
- ; (println depth)
-  (cond (winner? passive-sign board) (- 10 depth)
-        (winner? active-sign board) (- (- 10 depth))
+  (cond (winner? active-sign board) (- 10 depth)
+        (winner? passive-sign board) (- (- 10 depth))
         (is-tie? board) 0
         :else (best-score-at-depth depth (score-moves board active-sign passive-sign depth))))
+
+(defn player-sign [depth active-sign passive-sign]
+  (if (= (mod depth 2) 0)
+    active-sign
+    passive-sign))
 
 (defn score-moves [board active-sign passive-sign depth]
   (let [
        moves (get-free-places board)
-       scores (map #(get-score (put-sign-on-board board (str %) active-sign) (switch-signs active-sign) (switch-signs passive-sign) (inc depth)) moves)]
- ;      (println scores)
+       scores (map #(get-score (put-sign-on-board board (str %) (player-sign depth active-sign passive-sign)) active-sign passive-sign (inc depth)) moves)]
   (zipmap moves scores)))
 
 (defn minimax [board level active-sign passive-sign]
